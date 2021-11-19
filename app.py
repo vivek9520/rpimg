a1d8143a2f6567198fd847d3cadbe25642262b0a
VivekCodes <51886845+vivek9520@users.noreply.github.com>
2021-11-19 09:38:48 +0530

    Add files via upload
    new commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
